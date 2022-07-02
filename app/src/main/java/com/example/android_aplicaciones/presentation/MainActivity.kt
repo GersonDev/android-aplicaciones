e7f68b4c5386281fee13cea6c752200960a198bd
Gerson Cornelio Vargas <gersoncornelio1@gmail.com>
2022-07-01 22:26:44 -0500

    Diseñar una pantalla con varios botones 1. Venta de tarjeta 2. Ver registro de tarjetas

--- app/src/main/java/com/example/android_aplicaciones/presentation/MainActivity.kt
+++ app/src/main/java/com/example/android_aplicaciones/presentation/MainActivity.kt
@@ -12,52 +12,53 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.rememberNavController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
 import com.example.android_aplicaciones.R
-import com.example.android_aplicaciones.ui.theme.AndroidaplicacionesTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MenuPrincipalPantalla(onClickButtonOne = { /*TODO*/ }) {
+            val navController = rememberNavController()
+           NavHost(navController, startDestination = Screen.MenuPrincipal.route){
 
-            }
+           }
         }
     }
 }
 
-@Composable
-private fun MenuPrincipalPantalla(onClickButtonOne: () -> Unit, onClickButtonTwo: () -> Unit) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp),
-            painter = painterResource(id = R.drawable.metro1),
-            contentDescription = "Logo Linea1"
-        )
-        Button(onClick = onClickButtonOne, content = {
-            Text("Venta de Tarjetas")
-        })
-        Button(onClick = onClickButtonTwo, content = {
-            Text("Ver estado de Cuentas")
-        })
-        Button(onClick = {}, content = {
-            Text("Escanear tarjetas para ingresar al tren")
-        })
-        Button(onClick = {}, content = {
-            Text("Recargar Tarjeta")
-        })
+    @Composable
+    private fun MenuPrincipalPantalla(onClickButtonOne: () -> Unit, onClickButtonTwo: () -> Unit) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                painter = painterResource(id = R.drawable.metro1),
+                contentDescription = "Logo Linea1"
+            )
+            Button(onClick = onClickButtonOne, content = {
+                Text("Venta de Tarjetas")
+            })
+            Button(onClick = onClickButtonTwo, content = {
+                Text("Ver estado de Cuentas")
+            })
+            Button(onClick = {}, content = {
+                Text("Escanear tarjetas para ingresar al tren")
+            })
+            Button(onClick = {}, content = {
+                Text("Recargar Tarjeta")
+            })
+        }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun MenuPrincipalPreview() {
-    MenuPrincipalPantalla(onClickButtonOne = { /*TODO*/ }, onClickButtonTwo = {})
-}
+    @Preview(showBackground = true)
+    @Composable
+    fun MenuPrincipalPreview() {
+        MenuPrincipalPantalla(onClickButtonOne = { /*TODO*/ }, onClickButtonTwo = {})
+    }
+
